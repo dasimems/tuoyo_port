cfc967aabed8925ded58d447d4c594e83d58e469
dasimems <isaacseun63@gmail.com>
2023-01-18 14:18:41 +0100

    feat: added the functionality for mobile nav

---
+++ assets/scripts/index.js
@@ -0,0 +1,91 @@
+var pageActions = {
+    mobileOpen: false,
+}
+
+function Ele(element){
+    return document.querySelector(element);
+}
+
+function All(element){
+    return document.querySelectorAll(element);
+}
+
+function modalOpen(func){
+    var modalContainer = Ele(".modal");
+
+    modalContainer.style.display = "block";
+    if(func){
+        func();
+    }
+    
+    setTimeout(()=>{
+
+        modalContainer.style.opacity = "1";
+
+    }, 50)
+
+}
+
+function modalClose(func){
+    var modalContainer = Ele(".modal");
+
+    modalContainer.style.opacity = "0";
+
+    if(func){
+        func();
+    }
+    
+    setTimeout(()=>{
+
+        modalContainer.style.display = "none";
+
+
+    }, 600)
+
+}
+
+function openMobileNav() {
+    
+    modalOpen(()=>{
+        var mobileNavLink = Ele(".modal .mobile-nav-link");
+        
+        setTimeout(()=>{
+    
+            mobileNavLink.style.right = "0px";
+    
+        }, 50)
+    });
+
+
+
+}
+
+function closeMobileNav() {
+    
+    modalClose(()=>{
+        var mobileNavLink = Ele(".modal .mobile-nav-link");
+
+        mobileNavLink.style.right = "-500px";
+
+    });
+
+
+
+}
+
+
+
+window.addEventListener("load", () => {
+    var mobileLinkOpenMenu = Ele(".nav-bar .open-mobile-link-btn");
+    var mobileLinkCloseMenu = Ele(".mobile-nav-link .mobile-nav-link-header .close-mobile-nav-button");
+
+    mobileLinkOpenMenu.addEventListener("click", ()=> {
+        // console.log("clicked")
+        openMobileNav();
+    });
+
+    mobileLinkCloseMenu.addEventListener("click", ()=> {
+        closeMobileNav();
+    });
+
+})
